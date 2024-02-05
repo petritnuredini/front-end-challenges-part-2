651e8408b2ddeb1a6fb21c8f9a2b9f8135087120
petritnuredini <petrit.nuredini@gmail.com>
2024-02-05 15:27:10 +0100

    Finished blog preview card

--- blogpreviewcard/src/App.tsx
+++ blogpreviewcard/src/App.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import "./App.css";
+import ImageBlog from "./assets/images/illustration-article.svg";
+import ImageAvatar from "./assets/images/image-avatar.webp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <main className="app_container">
+      <article className="card_wrapper">
+        <div className="image_container">
+          <img src={ImageBlog} alt="Blog Image" className="blog_image" />
+        </div>
+        <button className="learning_category">Learning</button>
+
+        <p className="published_date">Published 21 Dec 2023</p>
+        <h1 className="blog_post_title">HTML & CSS foundations</h1>
+        <p className="blog_description">
+          These languages are the backbone of every website, defining structure,
+          content, and presantation.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className="profile_wrapper">
+          <div className="avatar_container">
+            <img
+              src={ImageAvatar}
+              alt="Profile Avatar"
+              className="profile_avatar_img"
+            />
+          </div>
+          <h2 className="profile_user_name">Greg Hooper</h2>
+        </div>
+      </article>
+    </main>
   );
 }
 
