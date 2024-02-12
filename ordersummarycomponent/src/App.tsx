dbb9bdeb1ec569d7c216ddb2af7d09b8a6defb44
petritnuredini <petrit.nuredini@gmail.com>
2024-02-12 21:27:17 +0100

    Working on design version

--- ordersummarycomponent/src/App.tsx
+++ ordersummarycomponent/src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import Card from "./components/Card";
+import BackgroundDesktop from "./assets/images/pattern-background-desktop.svg";
+import BackgroundMobile from "./assets/images/pattern-background-mobile.svg";
 
 function App() {
-  return <main className="App">Order summary</main>;
+  return (
+    <main className="order_summary_container">
+      <img
+        src={BackgroundDesktop}
+        alt="Background pattern desktop"
+        className="background_desktop"
+      />
+      <img
+        src={BackgroundMobile}
+        alt="Background pattern mobile"
+        className="background_mobile"
+      />
+      <Card />
+    </main>
+  );
 }
 
 export default App;
---
+++ ordersummarycomponent/src/components/Card.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./card-style.css";
+import IllustrationHero from "../assets/images/illustration-hero.svg";
+import MusicIcon from "../assets/images/icon-music.svg";
+
+const Card = () => {
+  return (
+    <section className="card_wrapper">
+      <div className="card_image_wrapper">
+        <img
+          src={IllustrationHero}
+          alt="Illustration Hero"
+          className="illustration_hero"
+        />
+      </div>
+      <div className="order_summary">
+        <h1 className="order_summary_title">Order Summary</h1>
+        <p className="order_description">
+          You can now listen o millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+        <div className="order_details">
+          <div className="music_icon_wrapper">
+            <img src={MusicIcon} alt="Music Icon" />
+          </div>
+          <div className="type_and_price">
+            <p className="order_type">Annual Plan</p>
+            <p className="order_price">$59.99/year</p>
+          </div>
+          <a href="#" className="change_link">
+            Change
+          </a>
+        </div>
+        <button className="process">Process to Payment</button>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
